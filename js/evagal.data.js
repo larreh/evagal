99530f5b85f4638b37e40a8dd7261340a8a6117c
larreh <larreh@gmail.com>
2021-12-25 20:30:17 -0800

    Added some dummy data

--- js/evagal.data.js
+++ js/evagal.data.js
@@ -1,8 +1,21 @@
 'use strict;'
 
-let events = 
+let events =
     [
-        {abc: 'awej'},
-        {abc: 'po2d'},
-        {def: '\0'}
+        {
+            name: 'Oklahoma City Bombing',
+            // remember javascript is dumb
+            begin: new Date(1995, 3, 19),
+            end: new Date(1995, 3, 19)
+        },
+        {
+            name: 'September 11th (2001)',
+            begin: new Date(2001, 8, 11),
+            end: new Date(2001, 8, 11)
+        },
+        {
+            name: "Don't ask, don't tell",
+            begin: new Date(1994, 1, 28),
+            end: new Date(2011, 8, 20)
+        }
     ];
